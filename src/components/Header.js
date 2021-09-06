628736545a7d38550564d943a8a8d9ab834d58d4
jrilley <j.rilley01@gmail.com>
2021-09-06 23:22:54 +0300

    Code formatting done

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://e7.pngegg.com/pngimages/192/97/png-clipart-logo-template-circle-advertising-circle-template-service.png" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            <div className="links">
+                <a href="#">Profile</a>
+            </div>
+            <div className="links">
+                <a href="#">Messages</a>
+            </div>
+            <div className="links">
+                <a href="#">News</a>
+            </div>
+            <div className="links">
+                <a href="#">Music</a>
+            </div>
+            <div className="links">
+                <a href="#">Settings</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
